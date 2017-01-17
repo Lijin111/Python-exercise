6f7158e096b599b45e12568b060006a099f4b519
Lijin <jin.li@capitalonline.net>
2017-01-17 10:38:37 +0800

    exercise try-except

---
+++ cardrun.py
@@ -0,0 +1,38 @@
+#!/user/bin/env python
+# -*- coding: UTF-8 -*-
+
+def safe_float(obj):
+    'safe version of float()'
+    try:
+        retval = float(obj)
+    except (ValueError,TypeError),diag:
+        retval = str(diag)
+    return retval
+
+def main():
+    'handles all the data processing'
+    log = open('cardlog.txt', 'w')
+    try:
+        ccfile = open('carddata.txt', 'r')
+    except IOError, e:
+        log.write('no txns this month\n')
+        log.close()
+        return
+
+    txns = ccfile.readlines()  #readlines()读取所有数据
+    ccfile.close()
+    total = 0.00
+    log.write('account log:\n')
+
+    for eachTxn in txns:
+        result = safe_float(eachTxn)
+        if isinstance(result,float):
+            total += result
+            log.write('data... processed\n')
+        else:
+            log.write('ignored: %s'% result)
+    print '$%.2f (new balance)' % (total)
+    log.close()
+
+if __name__ == '__main__':
+    main()
